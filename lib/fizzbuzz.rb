437c2bce27bd2df27dcfa54729ee47cb17d5f226
Mark <markjpoole@yahoo.com>
2020-07-09 18:28:28 +0100

    complete tasks

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,13 @@
-def fizzbuzz(number)
-  'fizz'
+class Integer
+  def fizzbuzz
+    if self % 3 == 0 and self % 5 == 0
+      'fizzbuzz'
+    elsif self % 3 == 0
+      'fizz'
+    elsif self % 5 == 0
+      'buzz'
+    else
+      self
+    end
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,13 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "fizz" for the number 3' do
+    expect(3.fizzbuzz).to eq 'fizz'
+  end
+  it 'returns "buzz" for the number 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" for the number 15' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
   end
 end
